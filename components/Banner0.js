8d20686e237c75a0a53b3f2eb9dec7352772b8ca
Steve Tokpo <stevendstokpo60@gmail.com>
2024-03-21 17:17:48 +0100

    no am

---
+++ components/Banner0.js
@@ -0,0 +1,127 @@
+import React, { useEffect } from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+
+import styles from '@/styles/Banner0.module.css'
+
+export default function Banner0() {
+    useEffect(() => {
+        const arrows = document.querySelectorAll('.arrows-animated-wrapper > img');
+        const delayBetweenArrows = 200;
+        const stayLitDuration = 600;
+
+        const animateArrows = () => {
+            arrows.forEach((arrow, index) => {
+                setTimeout(() => {
+                    arrow.style.animation = `fadeIn 0.5s forwards`;
+                }, (arrows.length - 1 - index) * delayBetweenArrows);
+            });
+
+            setTimeout(() => {
+                arrows.forEach((arrow, index) => {
+                    arrow.style.animation = `fadeOut 0.5s forwards`;
+                });
+                setTimeout(animateArrows, 500);
+            }, arrows.length * delayBetweenArrows + stayLitDuration);
+        };
+
+        animateArrows()
+    }, [])
+
+    return (
+        <div className={`flex justify-center items-center h-screen ${styles.darkBackground}`}>
+            <div className="flex flex-wrap items-center justify-center h-full md:justify-between">
+                <div className="flex flex-col justify-center items-start w-full md:w-1/2 md:items-center">
+                    <h1 data-w-id="4fd1001e-0883-42e7-af04-a12016ec15d7" className={` ${styles.textleft}`}>
+                        <Link href="/centre-de-depollution" className={styles.textwhity}>Centre de dépollution</Link>
+                    </h1>
+                    <div className="arrows-animated-wrapper">
+                        <Link href="/centre-de-depollution">
+                            <Image
+                                src="/images/arrow-outlone.svg"
+                                loading="lazy"
+                                alt="icon"
+                                className={`arrow-animated ${styles.reverseit}`}
+                                width={67}
+                                height={84}
+                                style={{display: "inline-bloack"}}
+                            />
+                            <Image
+                                src="/images/arrow-outlone.svg"
+                                loading="lazy"
+                                alt="icon"
+                                className={`arrow-animated-two ${styles.reverseit}`}
+                                width={67}
+                                height={84}
+                                style={{display: "inline-bloack"}}
+                            />
+                            <Image
+                                src="/images/arrow-outlone.svg"
+                                loading="lazy"
+                                alt="icon"
+                                className={`arrow-animated-three ${styles.reverseit}`}
+                                width={67}
+                                height={84}
+                                style={{display: "inline-bloack"}}
+                            />
+                            <Image
+                                src="/images/arrow-outlone.svg"
+                                loading="lazy"
+                                alt="icon"
+                                className={`arrow-animated-four ${styles.reverseit}`}
+                                width={67}
+                                height={84}
+                                style={{display: "inline-bloack"}}
+                            />
+                        </Link>
+                    </div>
+                </div>
+                <div className="flex flex-col justify-center items-end w-full md:w-1/2 md:items-center">
+                    <h1 data-w-id="4fd1001e-0883-42e7-af04-a12016ec15d7" className={`home-title ${styles.textright}`}>
+                        <Link href="/garage-atelier-mecanique" className={styles.textwhity}>Atelier mécanique</Link>
+                    </h1>
+                    <div className="arrows-animated-wrapper">
+                        <Link href="/garage-atelier-mecanique">
+                            <Image
+                                src="/images/arrow-outlone.svg"
+                                loading="lazy"
+                                alt="icon"
+                                className={`arrow-animated`}
+                                width={67}
+                                height={84}
+                                style={{display: "inline-bloack"}}
+                            />
+                            <Image
+                                src="/images/arrow-outlone.svg"
+                                loading="lazy"
+                                alt="icon"
+                                className={`arrow-animated-two`}
+                                width={67}
+                                height={84}
+                                style={{display: "inline-bloack"}}
+                            />
+                            <Image
+                                src="/images/arrow-outlone.svg"
+                                loading="lazy"
+                                alt="icon"
+                                className={`arrow-animated-three`}
+                                width={67}
+                                height={84}
+                                style={{display: "inline-bloack"}}
+                            />
+                            <Image
+                                src="/images/arrow-outlone.svg"
+                                loading="lazy"
+                                alt="icon"
+                                className={`arrow-animated-four`}
+                                width={67}
+                                height={84}
+                                style={{display: "inline-bloack"}}
+                            />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,32 @@
 import React, { useEffect } from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
-import gsap from 'gsap/dist/gsap'
 
-import styles from "@/styles/Index.module.css"
-import { APP_NAME, CANONICAL } from '../libs/constants'
+import { APP_NAME, CANONICAL } from '@/libs/constants'
+import Banner0 from '@/components/Banner0'
 
-function Home() {
+export default function CentreDeDepollution() {
+
     useEffect(() => {
-        const tl = gsap.timeline({ defaults: { ease: "power1.out" } })
-        tl.fromTo(`.${styles.columnTwo}`, 
-            { y: 50, opacity: 0, rotationX: -45, transformPerspective: 1000 },
-            { duration: 1, y: 0, opacity: 1, rotationX: 0, stagger: 0.2 }
-        )
-        tl.fromTo(".in-section-title",
-            { x: -100, opacity: 0 },
-            { duration: 1, x: 0, opacity: 1 }, "-=0.75"
-        )
-        tl.fromTo(".no-margin",
-            { x: 100, opacity: 0 },
-            { duration: 1, x: 0, opacity: 1 }, "-=0.75"
-        )
-        tl.fromTo(".primary-button",
-            { scale: 0.5, opacity: 0 },
-            { duration: 0.5, scale: 1, opacity: 1, ease: "back.out(1.7)" }, "-=0.5"
-        )
+        const arrows = document.querySelectorAll('.arrows-animated-wrapper > img');
+        const delayBetweenArrows = 200;
+        const stayLitDuration = 600;
+
+        const animateArrows = () => {
+            arrows.forEach((arrow, index) => {
+                setTimeout(() => {
+                    arrow.style.animation = `fadeIn 0.5s forwards`;
+                }, index * delayBetweenArrows);
+            });
+
+            setTimeout(() => {
+                arrows.forEach((arrow) => {
+                    arrow.style.animation = `fadeOut 0.5s forwards`;
+                });
+                setTimeout(animateArrows, 500);
+            }, arrows.length * delayBetweenArrows + stayLitDuration);
+        };
+
+        animateArrows()
     }, [])
 
     return (
@@ -32,44 +34,9 @@ function Home() {
             <Head>
                 <title>{APP_NAME}</title>
                 <meta name="robots" content="index, follow" />
-                <link rel="canonical" href={CANONICAL} />
+                <link rel="canonical" href={`${CANONICAL}/centre-de-depollution`} />
             </Head>
-            <div className={styles.darkBackground}>
-                <div className="pages-banner faq" style={{ backgroundImage: "url('/images/heros.jpg')", backgroundSize: "cover" }}>
-                    <div className="base-container w-container">
-                        <div data-w-id="bc04704d-d71c-ba15-06cc-f8a0fcfe9640" className="two-column-wrapper center">
-                            <div data-w-id="bc04704d-d71c-ba15-06cc-f8a0fcfe9641" className={styles.columnTwo}>
-                                <h2 className={`in-section-title text-white text-primary ${styles.metitle}`}>
-                                    Centre de dépollution
-                                </h2>
-                                <p className="no-margin text-white">
-                                    Préservez l'environnement et assurez la longévité de votre véhicule avec notre Centre de Dépollution, où les dernières technologies se rencontrent avec l'expertise pour un service de qualité supérieure.
-                                </p>
-                                <div className="button-wrapper-main">
-                                    <Link href="/centre-de-depollution" className="primary-button w-button">
-                                        Découvrez offre
-                                    </Link>
-                                </div>
-                            </div>
-                            <div data-w-id="bc04704d-d71c-ba15-06cc-f8a0fcfe9641" className={styles.columnTwo}>
-                                <h2 className={`in-section-title text-white text-primary ${styles.metitle}`}>
-                                    Garage & Atelier mécanique
-                                </h2>
-                                <p className="no-margin text-white">
-                                    Fiez-vous à l'excellence de notre Garage & Atelier Mécanique pour tous vos besoins en entretien et réparation. Nos mécaniciens certifiés s'engagent à offrir des prestations irréprochables pour que votre véhicule soit toujours en parfait état de marche.
-                                </p>
-                                <div className="button-wrapper-main">
-                                    <Link href="/garage-atelier-mecanique" className="primary-button w-button">
-                                        Explorez garage
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Banner0 />
         </>
     )
 }
-
-export default Home
