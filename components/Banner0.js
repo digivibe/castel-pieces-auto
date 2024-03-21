5ba89e456ea5a8ed287e353ae888dfdb54bb8227
Steve Tokpo <stevendstokpo60@gmail.com>
2024-03-21 18:22:18 +0100

    no am

--- components/Banner0.js
+++ components/Banner0.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import Link from 'next/link';
 import Image from 'next/image';
 
 import styles from '@/styles/Banner0.module.css'
@@ -31,12 +30,12 @@ export default function Banner0() {
     return (
         <div className={`flex justify-center items-center h-screen ${styles.darkBackground}`}>
             <div className="flex flex-wrap items-center justify-center h-full md:justify-between">
-                <div className="flex flex-col justify-center items-start w-full md:w-1/2 md:items-center">
+                <div className={`flex flex-col justify-center items-end w-full md:w-1/2 md:items-center ${styles.mm2}`}>
                     <h1 data-w-id="4fd1001e-0883-42e7-af04-a12016ec15d7" className={` ${styles.textleft}`}>
-                        <Link href="/centre-de-depollution" className={styles.textwhity}>Centre de dépollution</Link>
+                        <a href="/centre-de-depollution" className={styles.textwhity}>Centre de dépollution</a>
                     </h1>
                     <div className="arrows-animated-wrapper">
-                        <Link href="/centre-de-depollution">
+                        <a href="/centre-de-depollution">
                             <Image
                                 src="/images/arrow-outlone.svg"
                                 loading="lazy"
@@ -73,15 +72,15 @@ export default function Banner0() {
                                 height={84}
                                 style={{display: "inline-bloack"}}
                             />
-                        </Link>
+                        </a>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center items-end w-full md:w-1/2 md:items-center">
+                <div className={`flex flex-col justify-center items-end w-full md:w-1/2 md:items-center ${styles.mm2}`}>
                     <h1 data-w-id="4fd1001e-0883-42e7-af04-a12016ec15d7" className={`home-title ${styles.textright}`}>
-                        <Link href="/garage-atelier-mecanique" className={styles.textwhity}>Atelier mécanique</Link>
+                        <a href="/garage-atelier-mecanique" className={styles.textwhity}>Atelier mécanique</a>
                     </h1>
                     <div className="arrows-animated-wrapper">
-                        <Link href="/garage-atelier-mecanique">
+                        <a href="/garage-atelier-mecanique">
                             <Image
                                 src="/images/arrow-outlone.svg"
                                 loading="lazy"
@@ -118,7 +117,7 @@ export default function Banner0() {
                                 height={84}
                                 style={{display: "inline-bloack"}}
                             />
-                        </Link>
+                        </a>
                     </div>
                 </div>
             </div>
