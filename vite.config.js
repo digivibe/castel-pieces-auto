32195cf7e57b1dfd8fed22e969a5739912afe88a
Steve Tokpo <stevendstokpo60@gmail.com>
2024-03-26 11:27:55 +0100

    m

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        }
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                centre: resolve(__dirname, 'centre.html')
+            }
+        }
     }
-  }
 })
