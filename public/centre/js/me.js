9d7c1563263c815cc14167d75bf6b2613a4269b4
Steve Tokpo <stevendstokpo60@gmail.com>
2024-03-27 13:44:08 +0100

    k

---
+++ public/centre/js/me.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('widgetButton').addEventListener('click', function() {
+    var popupBox = document.getElementById('popupBox');
+    popupBox.classList.toggle('active'); // Bascule la classe "active" à chaque clic
+});
