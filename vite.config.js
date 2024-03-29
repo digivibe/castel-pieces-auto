16e2064e797491fe147f73ba76bf95210817a520
Steve Tokpo <stevendstokpo60@gmail.com>
2024-03-29 17:19:30 +0100

    l

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
+
+// Plugin Vite personnalisé pour servir des fichiers spécifiques pour des routes spécifiques
+function serveCustomHtml() {
+    return {
+        name: 'serve-custom-html',
+        configureServer(server) {
+            server.middlewares.use((req, res, next) => {
+                if (req.url === '/centre-de-depollution') {
+                    // Assurez-vous que le chemin est correct pour votre structure de projet
+                    const filePath = resolve(__dirname, 'centre.html');
+                    res.end(readFileSync(filePath));
+                } else {
+                    next();
+                }
+            });
+        }
+    };
+}
 
 export default defineConfig({
     plugins: [
         vue(),
+        serveCustomHtml() // Ajoutez votre plugin personnalisé ici
     ],
     resolve: {
         alias: {
@@ -20,4 +40,4 @@ export default defineConfig({
             }
         }
     }
-})
+});
