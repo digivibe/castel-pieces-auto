ffc1c57bd1ca145efa7e9b9518f0738168ab702c
Steve Tokpo <stevendstokpo60@gmail.com>
2024-04-03 11:10:05 +0100

    f

--- src/components/Banner0.vue
+++ src/components/Banner0.vue
@@ -3,21 +3,17 @@
         <div class="flex flex-wrap items-center justify-center h-full md:flex-nowrap md:justify-between">
             <div class="flex flex-col justify-center w-full px-4 md:w-1/2 md:items-center" :class="[mm2, textleft]">
                 <h2 class="text-5xl font-bold leading-tight" :class="[textleft, 'w-full', 'textwhity']">
-                    <a href="/centre.html" class="block">Centre<br />VHU</a>
+                    <a href="/centre-de-depollution" class="block">Centre<br />VHU</a>
                 </h2>
                 <p class="text-2xl mt-4">
-                    <a href="/centre.html" class="textwhity">centre de dépollution agréé</a>
+                    <a href="/centre-de-depollution" class="textwhity">centre de dépollution agréé</a>
                 </p>
                 <a href="/centre-de-depollution" class="mt-8 flex justify-center md:justify-start">
-                    <div class="arrows-animated-wrapper flex space-x-2">
-                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon"
-                            :class="`arrow-animated ${reverseit}`" width="67" height="84" />
-                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon"
-                            :class="`arrow-animated-two ${reverseit}`" width="67" height="84" />
-                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon"
-                            :class="`arrow-animated-three ${reverseit}`" width="67" height="84" />
-                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon"
-                            :class="`arrow-animated-four ${reverseit}`" width="67" height="84" />
+                    <div class="arrows-animated-wrappr flex space-x-2">
+                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" :class="`first ${reverseit}`" width="67" height="84" />
+                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" :class="`second ${reverseit}`" width="67" height="84" />
+                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" :class="`third ${reverseit}`" width="67" height="84" />
+                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" :class="`fourth ${reverseit}`" width="67" height="84" />
                     </div>
                 </a>
             </div>
@@ -29,18 +25,17 @@
                     <a href="/centre.html" class="textwhity">toutes marques</a>
                 </p>
                 <a href="/garage-atelier-mecanique" class="mt-8 flex justify-center md:justify-end">
-                    <div class="arrows-animated-wrapper flex space-x-2">
-                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" class="arrow-animated" width="67" height="84" />
-                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" class="arrow-animated-two" width="67" height="84" />
-                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" class="arrow-animated-three" width="67" height="84" />
-                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" class="arrow-animated-four" width="67" height="84" />
+                    <div class="arrows-animated-wrappr flex space-x-2">
+                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" class="fourth" width="67" height="84" />
+                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" class="third" width="67" height="84" />
+                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" class="second" width="67" height="84" />
+                        <img src="/images/arrow-outlone.svg" loading="lazy" alt="icon" class="first" width="67" height="84" />
                     </div>
                 </a>
             </div>
         </div>
     </div>
 </template>
-
 
 <script setup>
     // import { onMounted } from 'vue';
@@ -78,40 +73,96 @@
 </script>
 
 <style>
-@keyframes animateArrowRight {
-  0% {
-    transform: translateX(-50px);
-    opacity: 0;
-  }
-  50% {
-    opacity: 1;
-  }
-  100% {
-    transform: translateX(0px);
-    opacity: 0;
-  }
-}
-
-@keyframes animateArrowLeft {
-  0% {
-    transform: translateX(50px);
-    opacity: 0;
-  }
-  50% {
-    opacity: 1;
-  }
-  100% {
-    transform: translateX(0px);
-    opacity: 0;
-  }
-}
-
-.arrow-animated {
-  animation: animateArrowRight 2s infinite;
-}
-
-.arrow-animated-reverse {
-  animation: animateArrowLeft 2s infinite;
-}
-
+    .fourth {
+        animation: fourthgo 1.5s infinite;
+    }
+    @keyframes fourthgo {
+        0% {
+            opacity: 0;
+        }
+        20% {
+            opacity: 1;
+        }
+        40% {
+            opacity: 1;
+        }
+        60% {
+            opacity: 1;
+        }
+        80% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0;
+        }
+    }
+    .third {
+        animation: thirdgo 1.5s infinite;
+    }
+    @keyframes thirdgo {
+        0% {
+            opacity: 0;
+        }
+        20% {
+            opacity: 0;
+        }
+        40% {
+            opacity: 1;
+        }
+        60% {
+            opacity: 1;
+        }
+        80% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0;
+        }
+    }
+    .second {
+        animation: secondgo 1.5s infinite;
+    }
+    @keyframes secondgo {
+        0% {
+            opacity: 0;
+        }
+        20% {
+            opacity: 0;
+        }
+        40% {
+            opacity: 0;
+        }
+        60% {
+            opacity: 1;
+        }
+        80% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0;
+        }
+    }
+    .first {
+        animation: firstgo 1.5s infinite;
+    }
+    @keyframes firstgo {
+        0% {
+            opacity: 0;
+        }
+        20% {
+            opacity: 0;
+        }
+        40% {
+            opacity: 0;
+        }
+        60% {
+            opacity: 0;
+        }
+        80% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0;
+        }
+    }
 </style>
