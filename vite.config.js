7b42cdf4320cc40235f07096d3e14815fb2f4248
Steve Tokpo <stevendstokpo60@gmail.com>
2024-05-30 12:14:27 +0100

    maintenance feature

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,55 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { readFileSync } from 'fs'
 
-const maintenanceMode = true
+export default defineConfig(({ mode }) => {
+    
+    const env = loadEnv(mode, process.cwd())
+    
+    const maintenanceMode = env.VITE_MAINTENANCE_MODE === 'true'
 
-function serveCustomHtml() {
-    return {
-        name: 'serve-custom-html',
-        configureServer(server) {
-            server.middlewares.use((req, res, next) => {
-                console.log(`Maintenance mode: ${maintenanceMode}, Request URL: ${req.url}`)
-                const publicFiles = ['/css/Global.css', '/images/logo.svg', '/js/jquery.js', '/js/wf.js']
-                if (maintenanceMode && !publicFiles.includes(req.url) && req.url !== '/maintenance.html' && req.url !== '/maintenance') {
-                    res.writeHead(302, { Location: '/maintenance' })
-                    res.end()
-                } else if (!maintenanceMode && req.url === '/maintenance') {
-                    res.writeHead(302, { Location: '/' })
-                    res.end()
-                } else if (!maintenanceMode && req.url === '/centre-de-depollution') {
-                    const filePath = resolve(__dirname, 'centre.html')
-                    res.writeHead(200, { 'Content-Type': 'text/html' })
-                    res.end(readFileSync(filePath))
-                } else {
-                    next()
-                }
-            })
+    function serveCustomHtml() {
+        return {
+            name: 'serve-custom-html',
+            configureServer(server) {
+                server.middlewares.use((req, res, next) => {
+                    const publicFiles = ['/css/Global.css', '/images/logo.svg', '/js/jquery.js', '/js/wf.js']
+                    if (maintenanceMode && !publicFiles.includes(req.url) && req.url !== '/maintenance.html' && req.url !== '/maintenance') {
+                        res.writeHead(302, { Location: '/maintenance' })
+                        res.end()
+                    } else if (!maintenanceMode && req.url === '/maintenance') {
+                        res.writeHead(302, { Location: '/' })
+                        res.end()
+                    } else if (!maintenanceMode && req.url === '/centre-de-depollution') {
+                        const filePath = resolve(__dirname, 'centre.html')
+                        res.writeHead(200, { 'Content-Type': 'text/html' })
+                        res.end(readFileSync(filePath))
+                    } else {
+                        next()
+                    }
+                })
+            }
         }
     }
-}
 
-export default defineConfig({
-    plugins: [
-        vue(),
-        serveCustomHtml()
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        }
-    },
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                centre: resolve(__dirname, 'centre.html')
+    return {
+        plugins: [
+            vue(),
+            serveCustomHtml()
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            }
+        },
+        build: {
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, 'index.html'),
+                    centre: resolve(__dirname, 'centre.html')
+                }
             }
         }
     }
